dc6b7a56be5aaa985d5217d8062f668eab2dcfab
Changsik00 <lowmans00@gmail.com>
2018-09-30 20:28:28 +0900

    vue main 추가 및 테스트 - scss , router 확인

---
+++ WebRTC/src/components/Main.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        this is main
+        this is main
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            msg: 'Welcome to Your Vue.js App'
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+</style>
